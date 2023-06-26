da8f46bb2b41870f4e0527f51c1d999b9a1a89d9
m09238yx <yifan.xu-9@postgrad.manchester.ac.uk>
2023-06-26 11:45:59 +0100

    demo files

---
+++ Nao-Rbot-demo/translations/translation_en_US.ts
@@ -0,0 +1,68 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="en_US">
+    <context>
+        <name>behavior_1/behavior.xar:/Animated Say</name>
+        <message>
+            <source>Hello</source>
+            <comment>Text</comment>
+            <translation type="vanished">Hello</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Say</name>
+        <message>
+            <source>Hello</source>
+            <comment>Text</comment>
+            <translation type="vanished">Hello</translation>
+        </message>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>Hello, welcome to University of Manchetser, my name is nao. It's so nice to meet you all. I am sure we'll have a good time. </source>
+            <comment>Text</comment>
+            <translation type="unfinished">Hello, welcome to University of Manchetser, my name is nao. It's so nice to meet you all. I am sure we'll have a good time. </translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Say (1)</name>
+        <message>
+            <source>Hello</source>
+            <comment>Text</comment>
+            <translation type="vanished">Hello</translation>
+        </message>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>I am the first fully programed robot. I can connect to the internet using my wifi connection. I can recognize your face, answer your questions, play music, grab objects and even dance</source>
+            <comment>Text</comment>
+            <translation type="unfinished">I am the first fully programed robot. I can connect to the internet using my wifi connection. I can recognize your face, answer your questions, play music, grab objects and even dance</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Say (2)</name>
+        <message>
+            <source>Hello</source>
+            <comment>Text</comment>
+            <translation type="vanished">Hello</translation>
+        </message>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>Do you want me dance now? Touch my head. </source>
+            <comment>Text</comment>
+            <translation type="unfinished">Do you want me dance now? Touch my head. </translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Say (3)</name>
+        <message>
+            <source>Hello</source>
+            <comment>Text</comment>
+            <translation type="vanished">Hello</translation>
+        </message>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>I am going to dance now. </source>
+            <comment>Text</comment>
+            <translation type="unfinished">I am going to dance now. </translation>
+        </message>
+    </context>
+</TS>
